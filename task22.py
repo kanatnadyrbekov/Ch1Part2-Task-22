0d5317ab388362cba4f7d8ec1cf6b62addd832a1
kanat nadyrbekov <kanatnadyrbekov@gmail.com>
2019-12-30 09:55:05 +0600

    Ch1Part Task 22

---
+++ task22.py
@@ -0,0 +1,7 @@
+# Напишите функцию который будет конвертировать Фаренгейт в Цельсии и наоборот.
+a = int(input("Faren: "))
+Celsius = a-(32*5/9)
+print(Celsius)
+b = int(input("Celsius: "))
+Farenheit = a*9/5+32
+print(Farenheit)
